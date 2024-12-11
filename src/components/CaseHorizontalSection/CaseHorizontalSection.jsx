3025ad5d939778a2f0b7c4135b0442313abdfa2b
Oleh <eddrfgthyjn@gmail.com>
2024-12-11 21:42:19 +0200

    .

--- src/components/CaseHorizontalSection/CaseHorizontalSection.jsx
+++ src/components/CaseHorizontalSection/CaseHorizontalSection.jsx
@@ -1,35 +1,24 @@
-import styles from "./CaseHorizontalSection.module.css";
+import s from "./CaseHorizontalSection.module.css";
 
 export const CaseHorizontalSection = ({ cases }) => {
-  const data = [
-    {
-      title: "Challenges",
-      content: "Description of challenges goes here...",
-      image: cases.case_pictures_1,
-    },
-    {
-      title: "Involvement",
-      content: "Description of involvement goes here...",
-      image: cases.case_pictures_2,
-    },
-    {
-      title: "Results",
-      content: "Description of results goes here...",
-      image: cases.case_pictures_3,
-    },
-  ];
+  // Розпарсити JSON з поля "data-table-1"
+  const dataTable1 = cases["data-table-1"]
+    ? JSON.parse(cases["data-table-1"])
+    : {};
 
   return (
-    <div className={styles.container}>
-      <div className={styles.scrollContainer}>
-        {data.map((item, index) => (
-          <div key={index} className={styles.item}>
-            <h2 className={styles.title}>{item.title}</h2>
-            <p className={styles.content}>{item.content}</p>
-            <img className={styles.image} src={item.image} alt={item.title} />
+    <section className={s.section}>
+      <div className={s.container}>
+        <div className={s.horizontalBlocks}>
+          <div className={s.block}>
+            {Object.values(dataTable1).map((item, index) => (
+              <div key={index} className={s.item}>
+                {item}
+              </div>
+            ))}
           </div>
-        ))}
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
