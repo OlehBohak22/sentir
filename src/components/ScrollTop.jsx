4e8e62bba5fa599dea15ae8a20abd56d9941405f
Oleh <eddrfgthyjn@gmail.com>
2025-04-07 22:58:35 +0300

    refresh

--- src/components/ScrollTop.jsx
+++ src/components/ScrollTop.jsx
@@ -8,7 +8,7 @@ export const ScrollToTop = () => {
     window.scrollTo({
       top: 0,
       left: 0,
-      behavior: "instant", // або 'smooth'
+      behavior: "instant",
     });
   }, [pathname]);
 
